b44f68cb90969b87c2c29841effb691657cd39db
hardik <kantariyahardik618@ggamil.com>
2023-08-28 08:44:30 +0530

    cpp

---
+++ cont1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+   
+     using namespace std;
+
+int main()
+{
+    int countinue;
+    
+    for(char i=65;i<=90;i++)
+    {
+        if(i==5 || i==13)
+        {
+            countinue;
+        }
+        cout << i <<"\t";
+    }
+}
